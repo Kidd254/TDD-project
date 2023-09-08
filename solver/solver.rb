753c9942575047c7cc9b7a44bf6637969df2fd4c
Dachrono <andres_zamorano_@hotmail.com>
2023-09-07 20:28:54 -0600

    Add method fizzbuzz that meets the fizzbuzz tests

--- solver/solver.rb
+++ solver/solver.rb
@@ -10,4 +10,16 @@ class Solver
   def reverse(word)
     word.reverse
   end
+
+  def fizzbuzz(item)
+    if (item % 3).zero? && (item % 5).zero?
+      'fizzbuzz'
+    elsif (item % 3).zero?
+      'fizz'
+    elsif (item % 5).zero?
+      'buzz'
+    else
+      item.to_s
+    end
+  end
 end
