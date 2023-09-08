6fb5d5c50ca8787ab7f38c3738b7624a22c25574
Dachrono <andres_zamorano_@hotmail.com>
2023-09-07 19:48:52 -0600

    Add tests for fizzbuzz

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../solver/solver'
 
-describe Solver do
+RSpec.describe Solver do
   let(:solver) { Solver.new }
 
   describe '#factorial' do
@@ -21,5 +21,22 @@ describe Solver do
     it 'returns the reversed string' do
       expect(solver.reverse('hello')).to eq('olleh')
     end
+  end
+
+  describe '#fizzbuzz' do
+    it 'should returns "fizz" if the input is 3' do
+      expect(Solver.fizzbuzz(3)).to eq('fizz')
+  end
+
+  it 'should returns "buzz" if the input is 5' do
+    expect(Solver.fizzbuzz(5)).to eq('buzz')
+  end
+
+  it 'should returns "fizzbuzz" if the input is 15' do
+    expect(Solver.fizzbuzz(15)).to eq('fizzbuzz')
+  end
+
+  it 'should returns "2" if the input is 2' do
+    expect(Solver.fizzbuzz(2)).to eq('2')
   end
 end
